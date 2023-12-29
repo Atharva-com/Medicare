9167a817e46cf5fd88685ed128d9504587996af6
Your Name <github1504@gmail.com>
2023-12-29 16:16:47 +0530

    Footer section frontend

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.png'
 import { RiLinkedinFill } from 'react-icons/ri'
 import { AiFillYoutube, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai'
@@ -7,19 +7,19 @@ import { AiFillYoutube, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai'
 const socialLinks = [
   {
     path: '',
-    icons: <AiFillYoutube className='group-hover:text-white w-4 h-5'/>
+    icons: <AiFillYoutube className='group-hover:text-white w-4 h-5' />
   },
   {
     path: '',
-    icons: <AiFillGithub className='group-hover:text-white w-4 h-5'/>
+    icons: <AiFillGithub className='group-hover:text-white w-4 h-5' />
   },
   {
     path: '',
-    icons: <AiOutlineInstagram className='group-hover:text-white w-4 h-5'/>
+    icons: <AiOutlineInstagram className='group-hover:text-white w-4 h-5' />
   },
   {
     path: '',
-    icons: <RiLinkedinFill className='group-hover:text-white w-4 h-5'/>
+    icons: <RiLinkedinFill className='group-hover:text-white w-4 h-5' />
   },
 ]
 
@@ -84,7 +84,7 @@ const Footer = () => {
               Copyright @ {year} developed by Atharva . ALL Rights Reserved.
             </p>
 
-            <div>
+            <div className='flex items-center gap-3 mt-4'>
               {socialLinks.map((link, index) => {
                 return (
                   <Link to={link.path} key={index} className='w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
@@ -94,66 +94,66 @@ const Footer = () => {
               })}
             </div>
 
-            <div>
-              <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
-                Quick Links
-              </h2>
+          </div>
 
-              <ul>
-                {quickLinks01.map((item, index) => {
-                  return (
-                    <li key={index} className='mb-4'>
-                      <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
-                        {item.display}
-                      </Link>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+              Quick Links
+            </h2>
 
-            <div>
-              <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
-                I want to 
-              </h2>
+            <ul>
+              {quickLinks01.map((item, index) => {
+                return (
+                  <li key={index} className='mb-4'>
+                    <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
+                      {item.display}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
 
-              <ul>
-                {quickLinks02.map((item, index) => {
-                  return (
-                    <li key={index} className='mb-4'>
-                      <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
-                        {item.display}
-                      </Link>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+              I want to
+            </h2>
 
-            <div>
-              <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
-                Support Us
-              </h2>
+            <ul>
+              {quickLinks02.map((item, index) => {
+                return (
+                  <li key={index} className='mb-4'>
+                    <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
+                      {item.display}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
 
-              <ul>
-                {quickLinks03.map((item, index) => {
-                  return (
-                    <li key={index} className='mb-4'>
-                      <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
-                        {item.display}
-                      </Link>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+              Support Us
+            </h2>
 
+            <ul>
+              {quickLinks03.map((item, index) => {
+                return (
+                  <li key={index} className='mb-4'>
+                    <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
+                      {item.display}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
           </div>
 
         </div>
 
       </div>
-      
+
     </footer>
   )
 }
